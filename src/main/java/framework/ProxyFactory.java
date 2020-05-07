56139e52c0a1d4e7bd028fb3aa9e0f324bd37582
sunhuimingzxc <huiming.sun@aliyun.com>
2020-05-07 21:51:50 +0800

    第2次优化：cosummer通过注册中心获取privider地址，并以随机的方式调用privider

--- src/main/java/framework/ProxyFactory.java
+++ src/main/java/framework/ProxyFactory.java
@@ -9,20 +9,23 @@ import java.lang.reflect.Proxy;
 
 public class ProxyFactory {
 
+
+    //consummer通过注册中心获取privider地址，避免地址写死
+    //存在问题：更改通讯协议需要修改源码，比如从httpClient到NettyClient需要修改源码
     public static<T> T getProxy(final Class interfaceClass){
         return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class[]{interfaceClass}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 HttpClient httpClient = new HttpClient();
-
-                String result = httpClient.send("localhost",8080,
+                //通过注册中心获取privider地址，以地址随机的方式调用privider
+                URL url = RemoteMapRegister.random(interfaceClass.getName());
+                String result = httpClient.send(url.getHostname(),url.getPort(),
                         new Invocation(interfaceClass.getName(),method.getName(), method.getParameterTypes(), args));
 
                 return result;
             }
         });
     }
-
 
 
 }
--- src/main/java/register/RemoteMapRegister.java
+++ src/main/java/register/RemoteMapRegister.java
@@ -16,6 +16,7 @@ public class RemoteMapRegister {
     public static void regist(String interfaceName, URL url){
         List<URL> urls = Collections.singletonList(url);
         REGISTER.put(interfaceName, urls);
+        saveFile();//将注册列表存入文件，供consumer使用；dubbo使用的消息队列，而我们就用这种方式简化模拟一下
     }
 
     /**
@@ -24,11 +25,39 @@ public class RemoteMapRegister {
      * @return
      */
     public static URL random(String interfaceName){
+        REGISTER = getFile();
         List<URL> list = REGISTER.get(interfaceName);
         Random random = new Random();
         int n = random.nextInt(list.size());
         return list.get(n);
     }
 
+    private static void saveFile(){
+        try {
+            FileOutputStream fos = new FileOutputStream("temp.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(REGISTER);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Map<String, List<URL>> getFile(){
+        try {
+            FileInputStream fis = new FileInputStream("temp.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Map<String, List<URL>> map = (Map<String, List<URL>>) ois.readObject();
+            ois.close();
+            fis.close();
+            return map;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
