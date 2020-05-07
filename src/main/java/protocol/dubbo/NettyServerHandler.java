986e280232bda20275c9f5dd5583f3cf347a2aa6
sunhuimingzxc <huiming.sun@aliyun.com>
2020-05-07 21:58:36 +0800

    第3次提交：新增netty通讯协议；存在问题：修改通讯协议比如从http修改为netty，则需要修改源码

---
+++ src/main/java/protocol/dubbo/NettyServerHandler.java
@@ -0,0 +1,30 @@
+package protocol.dubbo;
+
+import framework.Invocation;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import provider.LocalRegister;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author tanghf
+ * @className protocol.dubbo.NettyServerHandler.java
+ * @createTime 2019/8/23 10:22
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        Invocation invocation = (Invocation) msg;
+        Class implClass = LocalRegister.get(invocation.getInterfaceName());
+        Method method = implClass.getMethod(invocation.getMethodName(), invocation.getParamTypes());
+        String invoke = (String) method.invoke(implClass, invocation.getParams());
+        System.out.println("Netty===============" + invoke);
+        ctx.writeAndFlush("Netty: " + invoke);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
